9f8c80cd1ea1f27af2ca19211bd6f2b2e4843fda
Israel de Alencar Silva <israel@iasdigitalgroup.com>
2020-01-30 14:46:21 -0300

    create a generic JsonHelper Create module and module information Mlodule Table script

---
+++ ias.Rebens/Helper/Config/ModuleInformation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ias.Rebens.Helper.Config
+{
+    public class ModuleInformation
+    {
+        public List<ModuleFields> fields { get; set; }
+        public List<string> files { get; set; }
+        public ModuleConfiguration configuration { get; set; }
+    }
+
+    public class ModuleFields
+    {
+        public string name { get; set; }
+        public string label { get; set; }
+        public string type { get; set; }
+        public List<ModuleFieldsData> data { get; set; }
+        public bool isRequired { get; set; }
+    }
+
+    public class ModuleFieldsData
+    {
+        public string name { get; set; }
+        public string value { get; set; }
+    }
+
+    public class ModuleConfiguration
+    {
+        public List<string> routeImports { get; set; }
+        public string routeConfig { get; set; }
+    }
+}
---
+++ ias.Rebens/Models/Module.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ias.Rebens
+{
+    public partial class Module
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Information { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+    }
+}
